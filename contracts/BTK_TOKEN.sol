b19e812344800caf9b9db645792aeac72abee510
sahatovbatyr <sahatovbatyr@gmail.com>
2022-03-03 21:50:29 +0500

    add Ownable modifier, releaseNewToken function

--- contracts/BTK_TOKEN.sol
+++ contracts/BTK_TOKEN.sol
@@ -7,10 +7,13 @@ import "hardhat/console.sol";
 contract BTK_TOKEN {
     string public name = "Batyr Token";
     string public symbol = "BTK";
+
     // The fixed amount of tokens stored in an unsigned integer type variable.
-    uint256 private totalToken = 1000000;
+    uint256 private totalToken = 0;
+
     // An address type variable is used to store ethereum accounts.
     address public owner;
+
     // A mapping is a key/value map. Here we store each account balance.
     mapping(address => uint256) balances;
 
@@ -20,12 +23,20 @@ contract BTK_TOKEN {
     event TransferEvent(address indexed _from, address indexed _to, uint256 _value);
     event ApprovalEvent(address indexed _owner, address indexed _spender, uint256 _value);
 
-
+    uint256  maxSupply=1000000*10^18;
+    
     constructor() {
         // The totalSupply is assigned to transaction sender, which is the account
         // that is deploying the contract.
         balances[msg.sender] = totalToken;
         owner = msg.sender;
+    }
+    
+    // Modifier to check that the caller is the owner of
+    // the contract.
+    modifier onlyOwner(){
+        require( msg.sender == owner, "Caller is not the owner");
+        _;
     }
 
     function transfer(address to, uint256 amount) external {
@@ -40,6 +51,15 @@ contract BTK_TOKEN {
 
     function getDecimals() public view returns (uint8) {
         return decimals;
+    }
+
+    //Release new tokens
+    function releaseNewToken( uint256 _newTokenAmount)  public onlyOwner returns (bool ) {
+
+        totalToken+=_newTokenAmount;
+        require( maxSupply>= totalToken, "It is forbidden to release tokens more than maxSupply");
+        return true;
+
     }
 
 
